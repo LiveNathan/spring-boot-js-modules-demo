038327423aabf03c249ada9d9567347c43d413e7
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-11-18 14:39:57 -0600

    first commit

---
+++ src/main/java/dev/nathanlively/springbootjsmodulesdemo/SpringBootJsModulesDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.springbootjsmodulesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsModulesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJsModulesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanlively/springbootjsmodulesdemo/SpringBootJsModulesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.springbootjsmodulesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJsModulesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
